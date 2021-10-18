0498c64eb88d65b871f033ca85d29bc2db35be36
Joaquim Soares <joaquim6399@gmail.com>
2021-10-18 02:44:29 +0100

    Exercise 1 completed

---
+++ exercise_1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "Welcome to becoming..." << endl
+	<< "...the best programmer I can become..."
+	<< endl << "...and learning C++." << endl;
+
+	return 0;
+}
